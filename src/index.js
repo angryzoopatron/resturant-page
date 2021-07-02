82f229da8377b7a843d034b63c77cb01c9264bec
angryzoopatron <81863211+angryzoopatron@users.noreply.github.com>
2021-07-02 13:57:02 -0400

    Update index.js
    Filler pictures added, notes at the bottom for what is next

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const mainDiv = document.querySelector('#content');
 
-// lazy html text instead of creating a ton of consts
+// lazy html strings instead of creating a ton of messy consts
 const myHtml = `
-<h1>Restaurant</h1>
-
+<h1>Click a tab below to navigate</h1>
 <div class="tab">
   <button class="tabLinks" onclick="openTab(event, 'Menu')">Menu</button>
   <button class="tabLinks" onclick="openTab(event, 'Contact')">Contact</button>
@@ -12,18 +11,23 @@ const myHtml = `
 
 <div id="Menu" class="tabContent">
   <h3>Menu</h3>
-  <p>Fill in menu items below</p>
+  <img src="https://picsum.photos/200">
 </div>
 
 <div id="Contact" class="tabContent">
   <h3>Contact</h3>
-  <p>Add fake contact info below</p>
+  <img src="https://picsum.photos/200?blur">
 </div>
 
 <div id="About" class="tabContent">
   <h3>About Us</h3>
-  <p>Add about us info below</p>
+  <img src="https://picsum.photos/200?grayscale">
 </div>
+
+<footer>
+  <p>Test feet</p>
+  <img id="pizza" src="https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg">
+</footer>
 `;
 
 // parse html text and add to div
@@ -32,7 +36,7 @@ const myFrag = document.createRange().createContextualFragment(myHtml);
 mainDiv.appendChild(myFrag);
 
 function openTab(evt, tabName) {
-    var i, tabContent, tabLinks;
+    let i, tabContent, tabLinks;
     tabContent = document.getElementsByClassName("tabContent");
     for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
@@ -44,3 +48,5 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+// add background images, food images & titles, and superfluous about & contact info
